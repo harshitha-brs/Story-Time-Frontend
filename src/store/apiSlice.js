5c439bcacac46ec43aa5c1f69ecad7981b7f1c9f
harshitha-brs <153584944+harshitha-brs@users.noreply.github.com>
2024-08-05 09:24:17 +0530

    Update apiSlice.js

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://story-time-backend-5vf4.onrender.com/",
+  baseUrl: "https://story-time-backend-um06.onrender.com/",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
  
